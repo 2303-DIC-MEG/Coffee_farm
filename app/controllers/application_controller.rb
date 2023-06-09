de65c20494b651fd36429ebc92a56fa3a0789c9a
yamamuro megumi <norain.norainbow.0223@gmail.com>
2023-06-10 07:13:33 +0900

    cancanによる管理者権限のroot設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,8 @@ class ApplicationController < ActionController::Base
     @search = Blog.ransack(params[:q])
     @search_blogs = @search.result
   end
+
+  rescue_from CanCan::AccessDenied do |_exception|
+    redirect_to root_path, alert: '画面を閲覧する権限がありません。'
+  end  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable
+  
+  has_secure_password
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
+  validates :password, presence: true, length: { minimum: 6, maximum: 255 }
 end
