e2e0e2f9cdff4431a9f025a7c6e25fdbf1e377d5
yamamuro megumi <norain.norainbow.0223@gmail.com>
2023-06-13 07:51:31 +0900

    userコントローラー追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :blogs
   has_one :profile
   has_many :favorites, dependent: :destroy
-
+  has_many :likes_blogs, through: :likes, source: :blog
   enum role: { Coffee_farmer: 0, Barista: 1 }
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable
