9335bf6e4e5044d0640610319c5d623eacd7d31d
yamamuro megumi <norain.norainbow.0223@gmail.com>
2023-06-14 06:11:23 +0900

    登録フォームセンター寄せに設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :role, profile_attributes: [:country, :address]])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :role])
   end
 end
