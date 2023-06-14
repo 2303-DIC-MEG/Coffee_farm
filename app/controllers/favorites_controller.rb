23c6034810fdb1add13806f40e5e2819c82f13b1
yamamuro megumi <norain.norainbow.0223@gmail.com>
2023-06-14 18:20:43 +0900

    blog作成者名表示

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,17 @@
 class FavoritesController < ApplicationController
   def index
     @favorites = current_user.favorites
+    favorites = Favorite.where(user_id: current_user.id).pluck(:blog_id)  # ログイン中のユーザーのお気に入りのblog_idカラムを取得
+    @favorite_list = Blog.find(favorites)     # blogsテーブルから、お気に入り登録済みのレコードを取得
   end
 
   def create
     favorite = current_user.favorites.create(blog_id: params[:blog_id])
-    redirect_to blogs_path, notice: "#{favorite.blog.user.name}さんのブログをお気に入り登録しました"
+    redirect_to blogs_path, notice: "#{favorite.blog.user.profile.name}さんのブログをお気に入り登録しました"
   end
 
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to blogs_path, notice: "#{favorite.blog.user.name}さんのブログをお気に入り解除しました"
+    redirect_to blogs_path, notice: "#{favorite.blog.user.profile.name}さんのブログをお気に入り解除しました"
   end
 end
