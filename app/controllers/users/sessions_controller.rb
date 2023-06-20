6b54c02619a84d2651c666ae9ab9189cfa0ac569
yamamuro megumi <norain.norainbow.0223@gmail.com>
2023-06-20 11:07:40 +0900

    ゲストログイン設定

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,9 +1,13 @@
 class Users::SessionsController < Devise::SessionsController
+  def guest_admin_sign_in
+    user = User.guest_admin
+    sign_in User.guest_admin
+    redirect_to after_sign_in_path_for(user), notice: 'ゲスト管理者としてログインしました。'
+  end
 
   def guest_sign_in
     user = User.guest
-    sign_in user
-    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+    sign_in User.guest
+    redirect_to after_sign_in_path_for(user), notice: 'ゲストユーザーとしてログインしました。'
   end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,20 @@ class User < ApplicationRecord
   def already_favorited?(blog)
     favorites.exists?(blog_id: blog.id)
   end
+
+  def self.guest_admin
+    find_or_create_by!(email: 'guest_admin@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "ゲストユーザー(管理者)"
+      user.admin = true
+    end
+  end
+
+  def self.guest_craftman
+    find_or_create_by!(email: 'guest_craftman@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "ゲストユーザー"
+      user.role = "Coffee farm"
+    end
+  end 
 end
