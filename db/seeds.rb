26987c79f01c13a0afbf2f404d4c32a9201e723c
yamamuro megumi <norain.norainbow.0223@gmail.com>
2023-06-21 02:16:39 +0900

    seedデータ修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ require 'faker'
     email: Faker::Internet.email,
     password: "password",
     password_confirmation: "password",
-    role: User.roles.keys.sample # ランダムな role の設定
+    role: User.roles.keys.sample 
   )
 
   # ブログデータの投入
@@ -19,28 +19,28 @@ require 'faker'
       user: user
     )
   end
-end
+
+  # プロフィールデータの投入
+  5.times do
+    profile = Profile.create!(
+      name: Faker::Name.name,
+      description: Faker::Lorem.paragraph(sentence_count: 2),
+      country: Faker::Address.country,
+      address: Faker::Address.full_address,
+      latitude: Faker::Address.latitude,
+      longitude: Faker::Address.longitude,
+      image:File.open("./public/images/sample.png"),
+      user: user
+    )
+  end
 
 
-# プロフィールデータの投入
-User.all.each do |user|
-  profile = Profile.create!(
-    name: Faker::Name.name,
-    description: Faker::Lorem.paragraph(sentence_count: 2),
-    country: Faker::Address.country,
-    address: Faker::Address.full_address,
-    latitude: Faker::Address.latitude,
-    longitude: Faker::Address.longitude,
-    image:File.open("./public/images/sample.png"),
-    user: user
-  )
-end
-
-
-# お気に入りデータの投入
-User.all.each do |user|
-  Blog.all.sample(3).each do |blog|
-    user.favorites.create!(blog_id: blog.id)
+  # お気に入りデータの投入
+  5.times do
+    favorite = Favorite.create!(
+      user: user,
+      blog: Blog.all.sample
+    )
   end
 end
 
