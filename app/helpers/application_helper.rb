cbef8858ff8b25200c1065cd66a5947da8f05720
yamamuro megumi <norain.norainbow.0223@gmail.com>
2023-06-21 09:28:04 +0900

    本番環境QRコードサイズ調整

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,5 @@
 module ApplicationHelper
   def render_qr_code(content, options = {})
-    size = options.delete(:size) || 200
     qrcode = RQRCode::QRCode.new(content)
     svg = qrcode.as_svg(options.merge(size: size)).html_safe
     svg_tag = content_tag(:div, svg, class: 'qr-code')
