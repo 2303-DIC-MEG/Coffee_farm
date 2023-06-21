afa8ae9574594c8c2654b5ef34e9a28bb82ea245
yamamuro megumi <norain.norainbow.0223@gmail.com>
2023-06-21 20:34:07 +0900

    Userモデルテスト実装

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,5 +24,35 @@ RSpec.describe User, type: :model do
         end
       end
     end
+    describe "emailカラムの確認" do
+      context "メールアドレスが登録されなかった場合" do
+        it "ユーザー登録ができない" do
+          user.email = ""
+          expect(user).not_to be_valid
+        end
+      end
+      context "メールアドレスの文字数が255以上だった場合" do
+        it "ユーザー登録ができない" do
+          user.email = "a" * 256 + '@example.com'
+          expect(user).not_to be_valid
+        end
+      end
+      context "メールアドレスが規定の形式と異なった場合" do
+        it "ユーザー登録ができない" do
+          user.email = "a" * 20
+          expect(user).not_to be_valid
+        end
+      end
+      context "メールアドレスが既存のものと重複した場合" do
+        it "ユーザー登録ができない" do
+          second_user = User.new(
+            name: "test2",
+            email: "test@example.com",
+            password: "password2"
+          )
+          expect(second_user).not_to be_valid
+        end
+      end
+    end
   end
 end
